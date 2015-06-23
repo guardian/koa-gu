12422c7616c8d00012311139cdf8c07d122331f7
Matt Osborn <matthew.osborn@theguardian.com>
2015-06-23 17:39:33 +0100

    fix config for s3

--- src/s3.js
+++ src/s3.js
@@ -2,7 +2,7 @@ var AWS = require('aws-sdk')
 var denodeify = require('denodeify')
 
 module.exports = function(cfg) {
-    var credentials = new AWS.SharedIniFileCredentials(cfg.AWSProfile ? {profile: cfg.AWSProfile} : {});
+    var credentials = new AWS.SharedIniFileCredentials(cfg.aws_profile ? {profile: cfg.aws_profile} : {});
     AWS.config.credentials = credentials;
     var s3 = new AWS.S3();
 

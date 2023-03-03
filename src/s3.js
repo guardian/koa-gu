b6393c873a698b591720fbde3d002510af28eb3a
Seán mór Clarke <sean.clarke@guardian.co.uk>
2023-03-03 08:02:49 +0000

    experiment with removing explicit credentials

--- src/s3.js
+++ src/s3.js
@@ -3,22 +3,7 @@ var denodeify = require('denodeify')
 
 module.exports = async function(cfg) {
 
-    async function getCredentials() {
 
-        let chain = new AWS.CredentialProviderChain();
-        chain.providers = [
-            function () { return new AWS.EC2MetadataCredentials()},
-            function () { return new AWS.SharedIniFileCredentials({profile: 'interactives'}); }
-        ]
-
-        let credentials = await chain.resolvePromise();
-        console.log(credentials);
-        return credentials;
-    
-    }
-
-
-    AWS.config.credentials = await getCredentials();
     var s3 = new AWS.S3();
 
     var exportFns = {};

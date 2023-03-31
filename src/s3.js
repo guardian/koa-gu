43398f46860bf83726cec7c286d3b9d7fe1e22df
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2023-03-31 11:46:15 +0100

    A few amendments after testing with gudocs

--- src/s3.js
+++ src/s3.js
@@ -1,41 +1,38 @@
-var AWS = require("aws-sdk")
-var denodeify = require("denodeify")
+var AWS = require("aws-sdk");
+var denodeify = require("denodeify");
 
 module.exports = async function (cfg) {
-  // var credentials = new AWS.SharedIniFileCredentials(cfg.aws_profile ? {profile: cfg.aws_profile} : {});
-  // AWS.config.credentials = credentials;
-
   AWS.CredentialProviderChain.defaultProviders = [
     function () {
-      return new AWS.EC2MetadataCredentials()
+      return new AWS.EC2MetadataCredentials();
     },
     function () {
       return new AWS.SharedIniFileCredentials({
-        profile: aws_profile ? aws_profile : "default",
-      })
+        profile: cfg.aws_profile ? cfg.aws_profile : "default",
+      });
     },
-  ]
+  ];
 
-  var chain = new AWS.CredentialProviderChain()
+  var chain = new AWS.CredentialProviderChain();
 
-  var chainpromise = chain.resolvePromise()
+  var chainpromise = chain.resolvePromise();
 
   chainpromise.then(
     function (credentials) {
       AWS.config.credentials = credentials;
     },
     function (err) {
-      console.log("Error: " + err.message)
+      console.log("Error: " + err.message);
     }
-  )
+  );
 
-  var s3 = new AWS.S3()
+  var s3 = new AWS.S3();
 
-  var exportFns = {}
+  const exportFns = {};
 
-  ;["putObject"].forEach(function (val) {
-    exportFns[val] = denodeify(s3[val].bind(s3))
-  })
+  ["putObject"].forEach(function (val) {
+    exportFns[val] = denodeify(s3[val].bind(s3));
+  });
 
-  return exportFns
-}
+  return exportFns;
+};
